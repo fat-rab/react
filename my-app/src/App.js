defe2023f68809f24aea54f944e8ab500ab9fc11
s1575478687 <1575478687@qq.com>
2021-01-05 20:26:10 +0800

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,40 +2,67 @@ import "./App.css";
 import React, { Component } from "react";
 import ListItem from "./components/listItem";
 
-const listData = [
-  {
-    id: 1,
-    name: "小米屌丝机",
-    price: 99,
-    stock: 20,
-  },
-  {
-    id: 2,
-    name: "华为爱国机",
-    price: 4000,
-    stock: 20,
-  },
-  {
-    id: 3,
-    name: "苹果高贵机",
-    price: 8000,
-    stock: 20,
-  },
-];
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      listData: [
+        {
+          id: 1,
+          name: "小米屌丝机",
+          price: 99,
+          stock: 20,
+          value: 4,
+        },
+        {
+          id: 2,
+          name: "华为爱国机",
+          price: 4000,
+          stock: 20,
+          value: 2,
+        },
+        {
+          id: 3,
+          name: "苹果高贵机",
+          price: 8000,
+          stock: 20,
+          value: 1,
+        },
+      ],
+    };
+  }
   renderList() {
     // if (listData.length === 0) {
     //   return <div className="text-content">购物车是空的</div>;
     // }
-    return listData.map((item) => {
-      return <ListItem key={item.id} data={item}></ListItem>;
+    return this.state.listData.map((item) => {
+      return (
+        <ListItem
+          key={item.id}
+          data={item}
+          onDelete={this.handleDetele}
+        ></ListItem>
+      );
     });
   }
+  handleDetele = (id) => {
+    const listData = this.state.listData.filter((item) => item.id !== id);
+    this.setState({
+      listData,
+    });
+  };
+  handleReset = () => {};
   render() {
     return (
       <div className="container">
-        {listData.length === 0 && (
+        <button
+          onClick={this.handleReset}
+          type="button"
+          className="btn btn-primary"
+        >
+          重置
+        </button>
+        {this.state.listData.length === 0 && (
           <div className="text-content">购物车是空的</div>
         )}
         {this.renderList()}
--- my-app/src/components/listItem.jsx
+++ my-app/src/components/listItem.jsx
@@ -1,32 +1,119 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import style from "./listItem.module.css";
 import classNames from "classnames/bind";
-import cn from "classnames";
+//import cn from "classnames";
 const cls = classNames.bind(style);
-let count = 0;
-class ListItem extends Component {
+//let count = 0;
+class ListItem extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      count: this.props.data.value,
+    };
+    /**
+     * 状态的类型是数组
+     * let _arr=[...this.state.arr,'1']
+     * this.setState({arr:_arr})
+     * 状态类型是对象
+     * const _obj=Object.assign({},this.state.obj,{a:1})
+     * const _obj={...this.state.obj,a:1}
+     * this.setState({obj:_obj})
+     */
   }
-  render() {
-    const _cn = cn({
-      "themed-grid-col-s": !count,
+  // checkCount() {
+  //   if (count < 0) count = 0;
+  // }
+  /**
+   * 传参方法
+   * onClick={this.handleDecrease.bind(this, this.props.data.id)}
+   * onClick={(e) => {this.handleDecrease(e,this.props.data.id)}} //如果需要使用事件对象，箭头函数方法需要手动传入
+   */
+  handleDecrease = () => {
+    this.setState(
+      {
+        count: this.state.count - 1,
+      },
+      () => {
+        if (this.state.count < 0) {
+          this.setState({
+            count: 0,
+          });
+        }
+      }
+    );
+    //使用箭头函数，否则this指向window（或者使用bind ，onClick={this.handleDecrease.bind(this)}）
+    //this.checkCount();
+  };
+  handleIncrease = () => {
+    this.setState({
+      count: this.state.count + 1,
     });
+  };
+  //避免在没有改变数据的情况下，修改虚拟DOM,建议使用PureComponent
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(this.props, nextProps, this.state, nextState);
+  //   if (this.state.count === nextState.count) return false;
+  //   if (this.props.data.id === nextProps.data.id) return false;
+  //   return true;
+  // }
+  render() {
+    // const _cn = cn({
+    //   "themed-grid-col-s": !count,
+    // });
+    console.log("render");
     return (
       <div className="row mb-3">
         <div className="col-6 themed-grid-col">
-          <span className={cls("title", "list-font")}>
-            {this.props.data.name}
-          </span>
-          {/* <span className={style.title}>{this.props.data.name}</span> */}
+          <span className={cls("title")}>{this.props.data.name}</span>
         </div>
         <div className="col-1 themed-grid-col">￥{this.props.data.price}</div>
-        <div className={`col-2 themed-grid-col ${_cn}`}>{count}</div>
-        {/* <div className={`col-2 themed-grid-col${count ? "" : "-s"}`}>
-          {count}
-        </div> */}
+        <div className={`col-2 themed-grid-col${this.state.count ? "" : "-s"}`}>
+          <button
+            onClick={() => {
+              this.handleDecrease(this.props.data.id);
+            }}
+            type="button"
+            className="btn btn-primary"
+          >
+            -
+          </button>
+          <span className={cls("digital")}>{this.state.count}</span>
+          <button
+            onClick={this.handleIncrease}
+            type="button"
+            className="btn btn-primary"
+          >
+            +
+          </button>
+        </div>
+        <div className="col-1 themed-grid-col">
+          ￥{this.props.data.price * this.state.count}
+        </div>
+        <div className="col-1 themed-grid-col">
+          <button
+            onClick={() => {
+              this.props.onDelete(this.props.data.id);
+            }}
+            type="button"
+            className="btn btn-danger btn-sm"
+          >
+            删除
+          </button>
+        </div>
       </div>
+      // <div className="row mb-3">
+      //   <div className="col-6 themed-grid-col">
+      //     <span className={cls("title", "list-font")}>
+      //       {this.props.data.name}
+      //     </span>
+      //     {/* <span className={style.title}>{this.props.data.name}</span> */}
+      //   </div>
+      //   <div className="col-1 themed-grid-col">￥{this.props.data.price}</div>
+      //   <div className={`col-2 themed-grid-col ${_cn}`}>{count}</div>
+      //   {/* <div className={`col-2 themed-grid-col${count ? "" : "-s"}`}>
+      //     {count}
+      //   </div> */}
+      // </div>
     );
   }
 }
