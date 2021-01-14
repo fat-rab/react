a9ee33eb7cf6322e16ac4dcab2a0aa16f26bd99c
s1575478687 <1575478687@qq.com>
2021-01-14 20:11:16 +0800

    reacthook

---
+++ react-hook/my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import HookCount from "./components/hookCount";
+function App() {
+  return (
+    <div className="App">
+      <HookCount></HookCount>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hook/my-app/src/components/hookCount.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+export default function HookCount() {
+  const maxTime = 10;
+  const [countDownText, setCountDownText] = useState("获取验证码");
+  const [timerNum, setTimerNum] = useState(maxTime);
+  const [boolCountDown, setBoolCountDown] = useState(false);
+  const refCountDown = useRef(null);
+  const handleClick = () => {
+    if (!boolCountDown) {
+      setBoolCountDown(true);
+    }
+  };
+  refCountDown.current = () => {
+    if (timerNum > 0) {
+      setTimerNum(timerNum - 1);
+    } else {
+      setTimerNum(maxTime);
+      setBoolCountDown(false);
+      setCountDownText("重新获取");
+    }
+  };
+  useEffect(() => {
+    if (boolCountDown) {
+      const timer = setInterval(() => {
+        refCountDown.current();
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [boolCountDown]);
+  return (
+    <button onClick={handleClick}>
+      {boolCountDown ? `${timerNum}s` : countDownText}
+    </button>
+  );
+}
