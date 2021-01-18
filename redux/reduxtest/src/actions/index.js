e6cf26f3160372e5745d756ccd259ad33ab326b7
s1575478687 <1575478687@qq.com>
2021-01-18 20:29:56 +0800

    redux

--- redux/reduxtest/src/actions/index.js
+++ redux/reduxtest/src/actions/index.js
@@ -1,1 +1,22 @@
-import { ADD_TODO, REMOVE_TODO } from "../actionTypes";
+let nextTodoId = 0
+export const addTodo = text => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = filter => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = id => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
--- redux/redux异步/src/index.js
+++ redux/redux异步/src/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import todoApp from "./reducers";
-let store = createStore(todoApp);
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+import Root from "./containers/Root";
+ReactDOM.render(<Root></Root>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
